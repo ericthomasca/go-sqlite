e68eff96d21462bdeb2aefb6cedf8a3f7ffc1ffb
Eric Thomas <eric@ericthomas.ca>
2023-01-24 21:54:52 -0330

    create basic calls, set up database

--- server/main.go
+++ server/main.go
@@ -16,7 +16,7 @@ func main() {
 		v1.GET("book/:id", getBooksById)
 		v1.POST("book", addBook)
 		v1.PUT("book/:id", updateBook)
-		v1.DELETE("person/:id", deleteBook)
+		v1.DELETE("book/:id", deleteBook)
 		v1.OPTIONS("book", options)
 	}
 
@@ -28,10 +28,24 @@ func getBooks(c *gin.Context) {
 }
 
 func getBooksById(c *gin.Context) {
-	id := c.Param("Id")
+	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{"message": "getBooksById " + id + " called"})
 }
 
 func addBook(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "addBook called"})
+}
 
+func updateBook(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "updateBook " + id + " called"})
+}
+
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "deleteBook " + id + " called"})
+}
+
+func options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "options called"})
 }
